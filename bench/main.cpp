ea3c58b72f9cac27ada002256f6c9d648f23f278
Ben Kolligs <benkolligs@gmail.com>
2022-04-01 15:39:31 -0400

    Added tests for the vector class

--- bench/main.cpp
+++ bench/main.cpp
@@ -1,8 +1,7 @@
 #include "main.h"
 
-int main(int argc, char const *argv[])
-{
-	mm::Vector3 v { 0.1, 0.0, 0.0 };
-	std::cout << v << std::endl;
-	return 0;
+int main(int argc, char const* argv[]) {
+    mm::Vector3 v{0.1, 0.0, 0.0};
+    std::cout << v << std::endl;
+    return 0;
 }
